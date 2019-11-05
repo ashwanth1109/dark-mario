975fc4250923831cc2dbf70d6fb4c48d266f94e8
Ashwanth A R <ashwanth1109@gmail.com>
2019-11-05 23:52:34 +0530

    Static background image render test

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,19 @@
+"use strict";
+
+import bgImg from "./sky.png";
+
 if (module.hot) module.hot.accept(() => window.location.reload());
+
+let bgLayer1;
+export function preload() {
+  bgLayer1 = loadImage(bgImg);
+}
 
 // Export the functions that are used by p5
 export function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
 }
 
-export function draw() {}
+export function draw() {
+  image(bgLayer1, 0, 0);
+}
