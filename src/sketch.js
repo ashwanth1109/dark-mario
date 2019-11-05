0c3bcb110c140e7814d83d4ff0725631172c0d59
Ashwanth A R <ashwanth1109@gmail.com>
2019-11-05 23:59:12 +0530

    Infinitely (auto) scrolling background

--- src/sketch.js
+++ src/sketch.js
@@ -5,6 +5,11 @@ import bgImg from "./sky.png";
 if (module.hot) module.hot.accept(() => window.location.reload());
 
 let bgLayer1;
+let x1 = 0;
+let x2;
+
+let scrollSpeed = 2;
+
 export function preload() {
   bgLayer1 = loadImage(bgImg);
 }
@@ -12,8 +17,16 @@ export function preload() {
 // Export the functions that are used by p5
 export function setup() {
   createCanvas(windowWidth, windowHeight);
+  x2 = width;
 }
 
 export function draw() {
-  image(bgLayer1, 0, 0);
+  image(bgLayer1, x1, 0, width, height);
+  image(bgLayer1, x2, 0, width, height);
+
+  x1 -= scrollSpeed;
+  x2 -= scrollSpeed;
+
+  if (x1 < -width) x1 = width;
+  if (x2 < -width) x2 = width;
 }
