d87c7b39109db7320ac068cea0efe8b26e9c032f
Ashwanth A R <ashwanth1109@gmail.com>
2019-11-05 19:23:45 +0530

    Setup p5 with full width canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+"use strict";
+
+import "normalize.css";
+console.log("Hello Dark Mario");
+
+const Sketch = p => {
+  let gray = 0;
+  p.setup = () => {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+  };
+  p.draw = () => {
+    p.background(gray);
+  };
+};
+
+new p5(Sketch);
