df63c590cf6249339790f5bb28091782c2110302
Ashwanth A R <ashwanth1109@gmail.com>
2019-11-09 09:36:48 +0530

    Decoupled horizontal movement to ground

--- src/sketch.js
+++ src/sketch.js
@@ -5,6 +5,9 @@ import ground from "./ground.png";
 import base from "./base.png";
 import bg4 from "./3.png";
 import normal from "./normal.png";
+import run1 from "./run1.png";
+import run2 from "./run2.png";
+import run3 from "./run3.png";
 
 if (module.hot) module.hot.accept(() => window.location.reload());
 
@@ -13,21 +16,28 @@ let bgLayer1;
 let bgLayer2;
 let bgLayer3;
 let bgLayer4;
+let run1Layer;
+let run2Layer;
+let run3Layer;
 let normalLayer;
 
 // x1 is the x position of image 1 | x2 for duplicate of image 1
 let x1 = 0;
 let x2;
-let y1 = 0;
-let y2;
+let g1 = 0;
+let g2;
 let mx = 100;
 let my;
 let isJumping = false;
 let goingUp = true;
+let run = [];
+let mario;
+let marioState = 0;
 
 // scroll speed refers to the step size by which the image
 // shifts on each re-draw
 let scrollSpeed = 2;
+let runSpeed = 3;
 
 // Export for all the functions that are used by p5
 // TODO: As this file grows, we can move out the functions to its
@@ -39,56 +49,69 @@ export function preload() {
   bgLayer2 = loadImage(ground);
   bgLayer3 = loadImage(base);
   bgLayer4 = loadImage(bg4);
+  run1Layer = loadImage(run1);
+  run2Layer = loadImage(run2);
+  run3Layer = loadImage(run3);
   normalLayer = loadImage(normal);
+  run = [run1Layer, run2Layer, run3Layer];
 }
 
 // setup our canvas
 export function setup() {
   createCanvas(windowWidth, windowHeight);
   x2 = width;
-  y2 = width;
-  my = height - 138;
+  g2 = width;
+  my = height - 150;
+  mario = run[marioState];
 }
 
 // draw method reference: https://p5js.org/reference/#/p5/draw
 export function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
-    mx += 3;
+    g1 -= 3;
+    g2 -= 3;
+    if (!isJumping) {
+      marioState = marioState + 1 > 2 ? 0 : marioState + 1;
+      mario = run[marioState];
+    } else {
+      mario = run[0];
+    }
+  } else {
+    mario = run[0];
   }
-  if (keyIsDown(LEFT_ARROW)) {
-    mx -= 3;
-  }
+  // if (keyIsDown(LEFT_ARROW)) {
+  //   mx -= 3;
+  // }
   if (!isJumping && keyIsDown(UP_ARROW)) {
     isJumping = true;
   }
   if (isJumping) {
-    if (goingUp && my > 470) my = my - 3;
-    else if (goingUp && my <= 470) goingUp = false;
-    else if (my < height - 138) my = my + 3;
+    if (goingUp && my > 400) my = my - 3;
+    else if (goingUp && my <= 400) goingUp = false;
+    else if (my < height - 150) my = my + 3;
     else {
       goingUp = true;
       isJumping = false;
     }
   }
 
-  console.log("DRAW");
+  console.log("DRAW", mario);
   // render our images in the respective positions
   image(bgLayer1, x1, 0, width, height); // initially on screen
   image(bgLayer1, x2, 0, width, height); // initially outside screen to the right
-  image(bgLayer2, 0, 0, width, height);
-  image(normalLayer, mx, my, 40, 80);
+  image(bgLayer2, g1, 0, width, height);
+  image(bgLayer2, g2, 0, width, height);
+  image(mario, mx, my, 50, 80);
 
   // // scroll both image and duplicate with each call of the draw function
   x1 -= scrollSpeed;
   x2 -= scrollSpeed;
-  y1 -= scrollSpeed * 2;
-  y2 -= scrollSpeed * 2;
 
   // // if one image goes out the canvas on the extreme left,
   // // swap it back to the right extreme to recycle the image
   if (x1 < -width + scrollSpeed) x1 = width;
   if (x2 < -width + scrollSpeed) x2 = width;
 
-  if (y1 < -width + scrollSpeed * 2) y1 = width;
-  if (y2 < -width + scrollSpeed * 2) y2 = width;
+  if (g1 < -width + runSpeed) g1 = width;
+  if (g2 < -width + runSpeed) g2 = width;
 }
